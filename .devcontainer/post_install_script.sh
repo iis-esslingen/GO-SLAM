7f76bc53cecfae5dc7f587caf3822e693c847d95
stihl-workstation <stihl-workstation@hs-esslingen.de>
2024-06-21 18:52:07 +0200

    Fixed typo of dep in dockerfile and small adjustments.

--- .devcontainer/post_install_script.sh
+++ .devcontainer/post_install_script.sh
@@ -1,15 +1,15 @@
 echo ">>> Start of post install script <<<"
+
 conda activate go-slam
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install evo --upgrade --no-binary evo
 python setup.py install
 
-pip install torch-scatter
-
+# pip install torch-scatter PyMCubes -> now inside of environment.yaml
 
 echo ">>> End of post install script <<<"
 
 
 #TODOs:
 # Add RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y DONE
-#pip install torch-scatter or conda install pytorch-scatter -c pyg DONE
+# pip install torch-scatter or conda install pytorch-scatter -c pyg DONE
