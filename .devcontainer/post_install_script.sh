118763d2cadc63a5f6836edd5545a0488ae13b26
Fabian Schmidt <fabian.schmidt@hs-esslingen.de>
2024-08-13 06:04:14 +0000

    update docker to specify paths

--- .devcontainer/post_install_script.sh
+++ .devcontainer/post_install_script.sh
@@ -3,9 +3,14 @@ set -eux
 
 echo ">>> Start of post install script <<<"
 
+git config --global --add safe.directory "*"
+
+DISPLAY_EXPORT="export DISPLAY=:0"
+
 conda activate go-slam
 pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 pip install evo --upgrade --no-binary evo
+cd go-slam
 python setup.py install
 
 # pip install torch-scatter PyMCubes -> now inside of environment.yaml
